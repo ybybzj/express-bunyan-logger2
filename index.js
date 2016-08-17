b03a6f571ac8308302598654c069cf99970b3edc
jackzj <ybybzj@gmail.com>
2016-08-17 13:39:24 +0800

    fix typo

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ module.exports.errorLogger = function (opts) {
         immediate = false,
         parseUA = true,
         excludes,
-        valueMappings,
+        valueMappingFn,
         genReqId = defaultGenReqId,
         levelFn = defaultLevelFn,
         includesFn,
